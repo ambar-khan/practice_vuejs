8fecd9f559d0b06ba2195e4b1fee3b475dbac85c
Ambar <ambarmogg@me.com>
2020-12-07 18:51:58 -0800

    add toggle

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Holiday Recipe Ingredients!'
+    message: 'Holiday Recipe Ingredients!',
+    showInfo: true
   },
   methods: {
     changeMessage: function () {
       console.log('changing message...');
       this.message = "Good Job!";
+    },
+    toggleInfo: function () {
+      console.log('toggling info...');
+      if (this.showInfo === true) {
+        this.showInfo = false;
+      } else {
+        this.showInfo = true;
+      }
     }
   }
-})
+});
